f16ac2ab562493eebfa1fb9531d2039577ce5a06
Ebrahim Mangera <ebrahimmangera9@gmail.com>
2023-06-27 01:01:05 +0000

    fixed typos in file names

--- 0x05-python-exceptions/2-safe_print_list_intergers.py
+++ 0x05-python-exceptions/2-safe_print_list_intergers.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    y = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end='')
-            y += 1
-        except (TypeError, ValueError):
-            continue
-    print()
-    return y
+    try:
+        i = 0
+        for j in range(x):
+            if isinstance(my_list[j], int):
+                print("{:d}".format(my_list[j]), end="")
+                i += 1
+        print()
+        return i
+    except IndexError:
+        print()
+        return i
