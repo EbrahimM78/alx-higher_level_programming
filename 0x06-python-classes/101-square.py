5bd54816a54a7c6bb7c720b0d295db23663ee62f
Ebrahim Mangera <ebrahimmangera9@gmail.com>
2023-06-27 22:32:21 +0000

    Additional tasks

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,77 +1,77 @@
 #!/usr/bin/python3
-"""Defines a singly linked list"""
+"""Defines a square bassed on 5-square.py"""
 
 
-class Node:
-    """Defines a node in a singly linked list"""
-
-    def __init__(self, data, next_node=None):
-        """Initializes the data"""
-
-        self.data = data
-        self.next_node = next_node
-
-    @property
-    def data(self):
-        """Gets data attribute"""
-
-        return (self.__data)
-
-    @data.setter
-    def data(self, value):
-        """Sets data attribute"""
-
-        if not isinstance(value, int):
-            raise TypeError('data must be an integer')
-        self.__data = value
-
-    @property
-    def next_node(self):
-        """Gets next_node attribute"""
-
-        return (self.__next_node)
-
-    @next_node.setter
-    def next_node(self, value):
-        """Sets next_node attribute"""
-
-        if (value is not None and not isinstance(value, Node)):
-            raise TypeError('next_node must be a Node object')
-        self.__next_node = value
-
-
-class SinglyLinkedList:
-    """Defines a singly linked list"""
-
-    def __init__(self):
-        """Initializes the data"""
-
-        self.head = None
+class Square:
+    """Represents a square"""
 
     def __str__(self):
-        """Prints the list"""
+        """Print square."""
 
-        printsll = ""
-        location = self.head
-        while location:
-            printsll += str(location.data) + "\n"
-            location = location.next_node
-        return printsll[:-1]
+        return self.pos_print()[:-1]
 
-    def sorted_insert(self, value):
-        """Inserts a node into a sorted linked list"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the data"""
 
-        new = Node(value)
-        if not self.head:
-            self.head = new
-            return
-        if value < self.head.data:
-            new.next_node = self.head
-            self.head = new
-            return
-        location = self.head
-        while location.next_node and location.next_node.data < value:
-            location = location.next_node
-        if location.next_node:
-            new.next_node = location.next_node
-        location.next_node = new
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Property of size of square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of square"""
+
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get the position"""
+
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position"""
+
+        if not isinstance(value, tuple):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def area(self):
+        """Return the current square area"""
+
+        return self.__size * self.__size
+
+    def pos_print(self):
+        """Print the square in position"""
+
+        pos = ""
+        if not self.size:
+            return "\n"
+        for w in range(self.position[1]):
+            pos += "\n"
+        for w in range(self.size):
+            for i in range(self.position[0]):
+                pos += " "
+            for j in range(self.size):
+                pos += "#"
+            pos += "\n"
+        return pos
+
+    def my_print(self):
+        """Print the square in position"""
+
+        print(self.pos_print(), end="")
